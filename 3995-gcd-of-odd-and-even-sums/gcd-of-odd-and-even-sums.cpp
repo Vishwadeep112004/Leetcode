195785f5bebe83b6c985a906927317e553b04846
Vishwadeep112004 <vishwadeep.sutar@walchandsangli.ac.in>
2026-07-15 10:28:53 +0530

    Time: 1 ms (21.15%) | Memory: 8.4 MB (99.40%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     int gcdOfOddEvenSums(int n) 
     {
-        int odd=((n)*(2-(n-1)*2))/2;
-        int even=((n)*(4-(n-1)*2))/2;
-        return gcd(odd,even);
+        return n;
     }
 };
