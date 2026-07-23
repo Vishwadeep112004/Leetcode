14a57929154c20ece995488a64c232b0f00e7052
Vishwadeep112004 <vishwadeep.sutar@walchandsangli.ac.in>
2026-07-23 20:20:08 +0530

    Time: 0 ms (100.00%) | Memory: 198.7 MB (85.00%) - LeetSync

--- 3824-number-of-unique-xor-triplets-i/number-of-unique-xor-triplets-i.cpp
+++ 3824-number-of-unique-xor-triplets-i/number-of-unique-xor-triplets-i.cpp
@@ -3,10 +3,17 @@ public:
     int uniqueXorTriplets(vector<int>& a) {
         int n=a.size();
         if(n<=2)return n;
-        int i=1;
-        while(i<=n)i<<=1;
-        i>>=1;
-        int x=(log(i)/log(2))+1;
-        return 1<<x;
+        // int i=1;
+        // while(i<=n)i<<=1;
+        // i>>=1;
+        // int x=(log(i)/log(2))+1;
+        // return 1<<x;
+        int cnt=0;
+        while(n)
+        {
+            n>>=1;
+            cnt++;
+        }
+        return 1<<cnt;
     }
 };
