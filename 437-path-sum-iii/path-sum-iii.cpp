c1b9379233335275359c45c36bea8be6e7c3de24
Vishwadeep112004 <vishwadeep.sutar@walchandsangli.ac.in>
2026-07-25 21:48:47 +0530

    Time: 12 ms (30.06%) | Memory: 21.8 MB (21.47%) - LeetSync

--- 437-path-sum-iii/path-sum-iii.cpp
+++ 437-path-sum-iii/path-sum-iii.cpp
@@ -1,40 +1,28 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
-    int give(TreeNode* root, long long k)
+    unordered_map<long long,int> mp;
+
+    int dfs(TreeNode* root,long long sum,int target)
     {
         if(!root)return 0;
-        int ans=0;
-        k-=root->val;
-        if(k==0)ans+=1;
-        ans+=give(root->left,k);
-        ans+=give(root->right,k);
+
+        sum+=root->val;
+
+        int ans=mp[sum-target];
+
+        mp[sum]++;
+
+        ans+=dfs(root->left,sum,target);
+        ans+=dfs(root->right,sum,target);
+
+        mp[sum]--;
+
         return ans;
     }
+
 public:
-    int pathSum(TreeNode* root, int k) 
+    int pathSum(TreeNode* root,int target)
     {
-        if(!root)return 0;
-        queue<TreeNode*> q;
-        q.push(root);
-        int ans=0;
-        while(!q.empty())
-        {
-            TreeNode* node=q.front();
-            q.pop();
-            ans+=give(node,k);
-            if(node->left)q.push(node->left);
-            if(node->right)q.push(node->right);
-        }
-        return ans;
+        mp[0]=1;
+        return dfs(root,0,target);
     }
 };
