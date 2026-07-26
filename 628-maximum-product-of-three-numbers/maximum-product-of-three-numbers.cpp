66b47908a27393f9e0d3aac3904b62e77e6b6116
Vishwadeep112004 <vishwadeep.sutar@walchandsangli.ac.in>
2026-07-26 10:58:29 +0530

    Time: 9 ms (42.43%) | Memory: 31.6 MB (46.18%) - LeetSync

--- 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.cpp
+++ 628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.cpp
@@ -3,9 +3,6 @@ public:
     int maximumProduct(vector<int>& a) {
         sort(a.begin(),a.end());
         int n=a.size();
-        int ans=a[n-1]*a[n-2]*a[n-3];
-        ans=max(ans,a[0]*a[1]*a[2]);
-        ans=max(ans,a[0]*a[1]*a[n-1]);
-        return ans;
+        return max(a[0]*a[1]*a[n-1],a[n-2]*a[n-3]*a[n-1]);
     }
 };
