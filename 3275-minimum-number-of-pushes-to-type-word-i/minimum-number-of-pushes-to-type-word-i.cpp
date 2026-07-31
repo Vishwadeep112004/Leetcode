0e8ad39788ecf80d4c80968b57fafd5c0e814194
Vishwadeep112004 <vishwadeep.sutar@walchandsangli.ac.in>
2026-07-31 10:52:30 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (15.38%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
@@ -5,13 +5,34 @@ public:
         int i=0;
         int cnt=0;
         int j=1;
-        while(i<n && i<8)cnt+=j,i++;
+        map<char,int> mp;
+        while(i<n && i<8)
+        {
+            if(mp.find(a[i])==mp.end()) mp[a[i]]=j; 
+            cnt+=mp[a[i]];
+            i++;
+        }
         j++;
-        while(i<n && i<8+8)cnt+=j,i++;
+        while(i<n && i<8+8)
+        {
+            if(mp.find(a[i])==mp.end()) mp[a[i]]=j; 
+            cnt+=mp[a[i]];
+            i++;
+        }
         j++;
-        while(i<n && i<8+8+8)cnt+=j,i++;
+        while(i<n && i<8+8+8)
+        {
+            if(mp.find(a[i])==mp.end()) mp[a[i]]=j; 
+            cnt+=mp[a[i]];
+            i++;
+        }
         j++;
-        while(i<n && i<8+8+8+8)cnt+=j,i++;
+        while(i<n && i<8+8+8+8)
+        {
+            if(mp.find(a[i])==mp.end()) mp[a[i]]=j; 
+            cnt+=mp[a[i]];
+            i++;
+        }
         return cnt;
     }
 };
