6e6f92289121f9ec200f1bd8b21f65c42b9e5f10
Vishwadeep112004 <vishwadeep.sutar@walchandsangli.ac.in>
2026-08-19 16:44:43 +0530

    Time: 104 ms (16.05%) | Memory: 80.5 MB (25.85%) - LeetSync

--- 1487-cinema-seat-allocation/cinema-seat-allocation.cpp
+++ 1487-cinema-seat-allocation/cinema-seat-allocation.cpp
@@ -1,40 +1,26 @@
 class Solution {
-
-    bool bs(vector<int>& a,int l,int r)
-    {
-        for(int i=0;i<a.size();i++)if(a[i]>=l && a[i]<=r)return false;
-        return true;
-    }
 public:
     int maxNumberOfFamilies(int n, vector<vector<int>>& temp) {
-        sort(temp.begin(),temp.end());
         map<int,vector<int>> mp;
-        for(vector<int> a:temp)mp[a[0]].push_back(a[1]);
-        int ans=0;
-        for(auto [row, a]:mp)
-        {
-            if(a.size()==1)
-            {
-                int ind=a[0];
-                if(ind<2 || ind>9)ans+=2;
-                else ans+=1;
-                continue;
-            }
-            if(a.size()>=7)continue;
-            bool x=bs(a,2,5);
-            bool y=bs(a,4,7);
-            bool z=bs(a,6,9);
-            if(x && z)ans+=2;
-            else if(x || y || z)ans+=1;
-        }
-        int pre=0;
+        for(auto a:temp)mp[a[0]].push_back(a[1]);
+
+        int ans=(n-mp.size())*2;
+
         for(auto [row,a]:mp)
         {
-            ans+=(row-pre-1)*2;
-            pre=row;
-        }
-        ans+=(n-pre)*2;
-        return ans;        
+            bool x=true,y=true,z=true;
 
+            for(int seat:a)
+            {
+                if(seat>=2 && seat<=5)x=false;
+                if(seat>=4 && seat<=7)y=false;
+                if(seat>=6 && seat<=9)z=false;
+            }
+
+            if(x && z)ans+=2;
+            else if(x || y || z)ans++;
+        }
+
+        return ans;
     }
 };
