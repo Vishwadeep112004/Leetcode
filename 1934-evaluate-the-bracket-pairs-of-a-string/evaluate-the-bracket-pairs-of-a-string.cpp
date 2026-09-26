b4f0eed7f0b6fd69442eaae9490ed7e5748ad5e8
Vishwadeep112004 <vishwadeep.sutar@walchandsangli.ac.in>
2026-09-26 09:40:40 +0530

    Time: 225 ms (7.79%) | Memory: 143 MB (23.05%) - LeetSync

---
+++ 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    string evaluate(string a, vector<vector<string>>& knowledge) 
+    {
+        map<string,string> mp;
+        for(vector<string> v:knowledge)
+        {
+            string key=v[0];
+            string val=v[1];
+            mp[key]=val;
+            // cout<<key<<val<<endl;
+        }
+
+        int i=0;
+        int n=a.size();
+        string ans="";
+        while(i<n)
+        {
+            while(i<n && a[i]!='(')
+            {
+                ans+=a[i];
+                i++;
+            }
+            string key="";
+            i++;
+            while(i<n && a[i]!=')')
+            {
+                key+=a[i];
+                i++;
+            }
+            if(key!="")
+            {
+                if(mp.find(key)!=mp.end())ans+=mp[key];
+                else ans+="?";
+            }
+            i++;
+        }
+        return ans;
+    }
+};
